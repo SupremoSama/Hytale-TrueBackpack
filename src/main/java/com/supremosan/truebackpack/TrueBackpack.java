9d35a810895642651f2f3d02c53463e5dd6a94af
SupremoSama <diegomatheus3093@gmail.com>
2026-02-16 20:33:49 -0300

    Partially resolved the issue where the UI does not update when equipping the backpack in the inventory.

--- src/main/java/com/supremosan/truebackpack/TrueBackpack.java
+++ src/main/java/com/supremosan/truebackpack/TrueBackpack.java
@@ -4,21 +4,12 @@ import com.hypixel.hytale.logger.HytaleLogger;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
 
-import javax.annotation.Nonnull;
-
 public class TrueBackpack extends JavaPlugin {
 
     private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
-    private static TrueBackpack instance;
-
-    @Nonnull
-    public static TrueBackpack get() {
-        return instance;
-    }
 
     public TrueBackpack(JavaPluginInit init) {
         super(init);
-        instance = this;
     }
 
     @Override
